54e5f6b19a909f3cfd829c1dee4446d56c2711c0
Fahri Nazarudin <fahrinzrdn@Fahris-MacBook-Pro.local>
2025-04-29 15:34:47 +0700

    feat(database): add initial database schema and tables

---
+++ BACKEND/models/user.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Product, { foreignKey: "userId" });
+    }
+  }
+  User.init(
+    {
+      userName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      phoneNumber: DataTypes.INTEGER,
+      password: DataTypes.STRING,
+      adress: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
